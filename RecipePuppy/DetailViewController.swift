ef1b9c6293f46f834f33889cbc33356ebf0de083
mnu <>
2018-12-02 09:04:08 +0100

    Navigation Bar

--- RecipePuppy/DetailViewController.swift
+++ RecipePuppy/DetailViewController.swift
@@ -39,10 +39,6 @@ class DetailViewController: UIViewController  {
         }
     }
     
-    @IBAction func btnBack_Touched(_ sender: UIButton) {
-        dismiss(animated: true, completion: nil)
-    }
-    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
